9847ca62634f710b8e9f921b308fe2606fd3df1f
sealandwu <32188098+sealandwu@users.noreply.github.com>
2023-03-06 14:18:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,6 +18,7 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
+sed -i '$a src-git luci-app-adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' feeds.conf.default
 
 git clone --depth=1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
