cdf6bab1355eb13b0226404b704ff962f7012cd9
sealandwu <32188098+sealandwu@users.noreply.github.com>
2025-08-21 18:22:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,12 +21,13 @@ echo 'src-git helloworld https://github.com/fw876/helloworld;master' >>feeds.con
 #echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
 #echo 'src-git luci-app-adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' >>feeds.conf.default
 #sed -i '$a src-git luci-app-adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' feeds.conf.default
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
+###//20250821
+###echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+###echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
 
-git clone --depth=1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
-git clone https://github.com/1wrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+###git clone --depth=1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+###git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+###git clone https://github.com/1wrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
+###git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 #git clone https://github.com/godros/luci-app-godproxy.git package/luci-app-godproxy
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+###git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
